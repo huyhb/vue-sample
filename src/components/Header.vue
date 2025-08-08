7aafcbc78601b3cb85c6b33b940d011b75aa9b58
huyhb <huyhb@unit.com.vn>
2025-08-08 16:04:11 +0700

    add buttons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,8 @@
     <div >
       <h1>BrandName</h1>
       <div>
-        <a href="#" class="hover:text-blue-600">Home</a>
-        <a href="#" class="hover:text-blue-600">Features</a>
+        <a href="https://sign-server-dev.unit.vn/txnid" class="hover:text-blue-600">Server Unit</a>
+        <a href="https://vue-sample-2p2.pages.dev/test" class="hover:text-blue-600">Server Vue</a>
         <a href="#" class="hover:text-blue-600">Testimonials</a>
         <a href="#" class="hover:text-blue-600">Contact</a>
       </div>
